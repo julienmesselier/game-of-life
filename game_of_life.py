cc15fb6ea5cf25d3622ee17f5bd7ae07b16657f0
julien <julien.messelier@sgcib.com>
2017-11-01 10:37:49 +0100

    added computed neighbors method

--- game_of_life.py
+++ game_of_life.py
@@ -1,6 +1,13 @@
 import numpy as np
 
-def iterate(World):
-    return      np.array([[0, 0, 0],
-                          [0, 0, 0],
-                          [0, 0, 0]])
+
+def compute_number_of_neighbours(world):
+    return (world[0:-2, 0:-2] + world[0:-2, 1:-1] + world[0:-2, 2:] +
+            world[1:-1, 0:-2] + world[1:-1, 2:] +
+            world[2:  , 0:-2] + world[2:, 1:-1] + world[2:, 2:])
+
+
+def iterate(world):
+    world *= 0
+    world[0, :] = world[-1, :] = world[:, 0] = world[:, -1] = 0
+    return world
--- test_game_of_life.py
+++ test_game_of_life.py
@@ -6,4 +6,36 @@ class TestGameOfLife(TestCase):
         world = np.array([[0, 0, 0],
                          [0, 0, 0],
                          [0, 0, 0]])
-        np.testing.assert_array_equal(world,iterate(world))
+        np.testing.assert_array_equal(world, iterate(world))
+
+
+    def test_world_borders_remains_empty_after_iteration(self):
+        world = np.array([[0, 1, 0],
+                         [1, 1, 1],
+                         [0, 1, 0]])
+
+        world_iterated = iterate(world)
+        north_border = world_iterated[0, :]
+        south_border = world_iterated[-1, :]
+        east_border = world_iterated[:, 0]
+        west_border = world_iterated[:, -1]
+
+        np.testing.assert_array_equal(0, north_border)
+        np.testing.assert_array_equal(0, south_border)
+        np.testing.assert_array_equal(0, east_border)
+        np.testing.assert_array_equal(0, west_border)
+
+    def test_cell_with_exactly_3_neighbors_remains_alive_after_iteration(self):
+        world = np.array([[1, 0, 0],
+                          [1, 1, 1],
+                          [0, 0, 0]])
+#        self.assertEqual(1, iterate(world)[1, 1])
+
+    def test_neighbors_computation(self):
+        world = np.array([[1, 0, 0],
+                          [1, 1, 1],
+                          [0, 0, 0]])
+
+        expected_neighbors = np.array([[3]])
+
+        np.testing.assert_array_equal(expected_neighbors, compute_number_of_neighbours(world))
